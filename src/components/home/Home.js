1f888cc1e2f18b3cd57767c1c3db9dc520311f7b
shiv <shiv@revinfotech.com>
2019-02-22 17:32:28 +0530

    latest

--- src/components/home/Home.js
+++ src/components/home/Home.js
@@ -13,10 +13,19 @@ import TextField from '@material-ui/core/TextField';
 import DialogTitle from '@material-ui/core/DialogTitle';
 import DialogActions from '@material-ui/core/DialogActions';
 import Grid from '@material-ui/core/Grid';
+import Grow from '@material-ui/core/Grow';
+import Chip from '@material-ui/core/Chip'
+
+import Snackbar from '@material-ui/core/Snackbar';
 
 // Icons
 import MenuIcon from '@material-ui/icons/Menu';
 
+// AXIOS
+import axios from 'axios';
+import Card from '@material-ui/core/Card'
+import CardContent from "@material-ui/core/CardContent";
+import CardActionArea from "@material-ui/core/CardActionArea";
 
 export default class Home extends Component {
 
@@ -27,12 +36,36 @@ export default class Home extends Component {
             loginModal: false,
             signupModal: false,
             login: {
-                email: '',
-                password: ''
-            }
-        }
+                email: 'shiv@gmail.com',
+                password: 'Hello@123'
+            },
+            message: '',
+            snackbar: false,
+            user: {
+                token: '',
+                name: ''
+            },
+            questions: []
+
+        };
     }
 
+    componentWillMount() {
+        this.getAccessToken();
+        this.getQuestions();
+    }
+
+
+    getAccessToken = () => {
+        const token = localStorage.getItem('token');
+        const name = localStorage.getItem('name');
+        if (token) {
+            let userObj = {...this.state.user};
+            userObj.token = token;
+            userObj.name = name;
+            this.setState({user: userObj})
+        }
+    };
 
     toggleDrawer = (open) => () => {
         this.setState({
@@ -43,7 +76,13 @@ export default class Home extends Component {
     toggleLoginModal = (open) => () => {
         this.setState({
             loginModal: open
-        })
+        });
+        // this.setState({
+        //     login: {
+        //         email: '',
+        //         password: ''
+        //     }
+        // })
     };
 
     toggleSignupModal = (open) => () => {
@@ -52,13 +91,72 @@ export default class Home extends Component {
         })
     };
 
-    changeLogin = () => {
-
+    changeLogin = (event) => {
+        const loginObj = {...this.state.login};
+        loginObj[[event.target.name]] = event.target.value;
+        this.setState({login: loginObj})
     };
 
     submitLogin = () => {
 
+        const body = {
+            email: this.state.login.email,
+            password: this.state.login.password
+        };
+
+        axios.post('http://localhost:3000/api/user/login', body)
+            .then((response) => {
+                localStorage.setItem('token', response.data.token);
+                localStorage.setItem('name', response.data.name);
+
+                this.getAccessToken();
+
+                this.setState({message: response.data.message}, () => {
+                    this.setState({
+                        snackbar: true, login: {
+                            email: '',
+                            password: ''
+                        }, loginModal: false
+                    });
+                    setTimeout(() => {
+                        this.setState({snackbar: false});
+                    }, 2000)
+                })
+            })
+            .catch((error) => {
+                this.setState({message: error.response.data.message}, () => {
+                    this.setState({snackbar: true});
+                    setTimeout(() => {
+                        this.setState({snackbar: false});
+                    }, 2000)
+                })
+            })
     };
+
+    getQuestions = () => {
+        axios.get('http://localhost:3000/api/question/1')
+            .then(response => {
+                console.log(response.data)
+
+                let questionsObj = [...this.state.questions];
+                const mergedQuestions = questionsObj.concat(response.data.data);
+
+                this.setState({questions: mergedQuestions})
+
+            })
+            .catch(error => {
+                console.log(error.response)
+            })
+    };
+
+    // SLICING
+    sliceTitle = (str) => {
+        return str.slice(0, 50)
+    };
+
+    sliceDescription = (str) => {
+        return str.slice(0,350)
+    }
 
     render() {
         return (
@@ -73,10 +171,54 @@ export default class Home extends Component {
                         <Typography variant="h6" color="inherit" style={{flexGrow: 1}} align={'center'}>
                             <span> <i className="fas fa-chess-queen"> </i> Overflow </span>
                         </Typography>
-                        <Button color="inherit" onClick={this.toggleLoginModal(true)}>Login</Button>
-                        <Button color="inherit" onClick={this.toggleSignupModal(true)}>Signup</Button>
+                        {
+                            this.state.user.token ?
+                                <Button color="inherit">{this.state.user.name}</Button>
+                                :
+                                <div>
+                                    <Button color="inherit" onClick={this.toggleLoginModal(true)}>Login</Button>
+                                    <Button color="inherit" onClick={this.toggleSignupModal(true)}>Signup</Button>
+                                </div>
+                        }
+
                     </Toolbar>
                 </AppBar>
+
+                <Grid container>
+                    {this.state.questions.map((question, index) => {
+                        return <Grow
+                            in={true}
+                            style={{transformOrigin: '0 0 0'}}
+                            {...(true ? {timeout: 3000} : {})}
+                         key={index}>
+
+                            <Grid item xs={12} sm={6} md={4} lg={3} style={{padding: '10px'}}>
+                                <Card style={{height: '100%'}}>
+                                    <CardContent>
+                                        <Typography component="h2">
+                                            <strong>{this.sliceTitle(question.title)} ...</strong>
+                                        </Typography>
+                                        <Typography component="p">
+                                            {this.sliceDescription(question.description)} ...
+                                        </Typography>
+                                        {
+                                            question.tags.map((tag => {
+                                                return  <Chip
+                                                    label={tag.name}
+                                                    onClick={() => {}}
+                                                    color="secondary"
+                                                    style={{fontSize: '10px', padding: 0}}
+                                                />
+                                            }))
+                                        }
+
+                                    </CardContent>
+                                </Card>
+                            </Grid>
+                        </Grow>
+                    })}
+                </Grid>
+
 
                 {/* DRAWER */}
                 <Drawer open={this.state.leftDrawer} onClose={this.toggleDrawer(!this.state.leftDrawer)}>
@@ -104,6 +246,7 @@ export default class Home extends Component {
                                     margin="dense"
                                     label="Email Address"
                                     type="email"
+                                    name="email"
                                     fullWidth
                                     onChange={this.changeLogin}
                                     value={this.state.login.email}
@@ -114,6 +257,7 @@ export default class Home extends Component {
                                     margin="dense"
                                     label="Password"
                                     type="password"
+                                    name="password"
                                     fullWidth
                                     onChange={this.changeLogin}
                                     value={this.state.login.password}
@@ -122,7 +266,7 @@ export default class Home extends Component {
                         </Grid>
                     </DialogContent>
                     <DialogActions>
-                        <Button variant="contained" onClick={this.handleClose} color="primary">
+                        <Button variant="contained" onClick={this.submitLogin} color="primary">
                             Login
                         </Button>
                         <Button variant="contained" onClick={() => {
@@ -188,6 +332,19 @@ export default class Home extends Component {
                         </Button>
                     </DialogActions>
                 </Dialog>
+
+                {/*SNACKBAR*/}
+                <Snackbar
+                    anchorOrigin={{vertical: 'top', horizontal: 'right'}}
+                    open={this.state.snackbar}
+                    variant="warning"
+                    onClose={this.handleClose}
+                    ContentProps={{
+                        'aria-describedby': 'message-id',
+                    }}
+                    message={<span id="message-id">{this.state.message}</span>}
+                />
+
             </div>
 
         )
